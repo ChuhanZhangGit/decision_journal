c05a0da8f36c48ba5caad869b640aea6afdbf2bd
Yilin Xu <xu.yilin@northeastern.edu>
2020-11-30 23:04:19 -0500

    add create activity

---
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/CreateActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.decisionjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
