ef57dcb95bbb2801ead717adccb205909bce4a0e
chuhan95 <zhang.chuh@husky.neu.edu>
2020-12-07 17:53:24 -0500

    collapsed actionbar draw for calendar view

--- app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
@@ -1,16 +1,18 @@
 package edu.neu.madcourse.decisionjournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import java.sql.Date;
+import com.google.android.material.appbar.AppBarLayout;
+
 import java.time.LocalDate;
-
-import edu.neu.madcourse.decisionjournal.model.DecisionEnum;
-import edu.neu.madcourse.decisionjournal.model.EmoEnum;
-import edu.neu.madcourse.decisionjournal.model.Record;
 
 
 /**
@@ -19,12 +21,55 @@ import edu.neu.madcourse.decisionjournal.model.Record;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private AppDatabase database;
+    private LinearLayout pickDateArea;
+    private AppBarLayout appBarLayout;
+    private CalendarView calendarView;
+    boolean appbarExpanded = false;
+
+    private LocalDate selectedDate;
+    private TextView selectedDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         database = AppDatabase.getDatabase(getApplicationContext());
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        pickDateArea = findViewById(R.id.pick_date_area);
+        appBarLayout = findViewById(R.id.app_bar_layout);
+        calendarView = findViewById(R.id.calendar_view);
+        selectedDateTextView = findViewById(R.id.toolbar_date_display);
+        appBarLayout.setExpanded(appbarExpanded);
+
+        setupCalendarListener();
+        // Initial calendar view to select today's date.
+        calendarView.setDate(0);
+        calendarView.setDate(System.currentTimeMillis());
+    }
+
+    private void setupCalendarListener() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                selectedDate = LocalDate.of(i, i1+1, i2);
+                setToolbarDate();
+            }
+        });
+    }
+
+    private void setToolbarDate() {
+        this.selectedDateTextView.setText(selectedDate.toString());
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.pick_date_area:
+                appbarExpanded = !appbarExpanded;
+                appBarLayout.setExpanded(appbarExpanded, true);
+                break;
+        }
     }
 }
