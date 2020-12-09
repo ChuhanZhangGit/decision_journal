8134d6536765628b783f1ca4595dd6ab8cd025e2
chuhan95 <zhang.chuh@husky.neu.edu>
2020-12-08 21:15:44 -0500

    recycler view connected with DB; Screen B initial implementation done.

--- app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
@@ -3,11 +3,10 @@ package edu.neu.madcourse.decisionjournal;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.LinearLayout;
@@ -16,13 +15,9 @@ import android.widget.TextView;
 import com.google.android.material.appbar.AppBarLayout;
 
 import java.sql.Date;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.List;
 
 import edu.neu.madcourse.decisionjournal.dao.AsyncRecordRepository;
-import edu.neu.madcourse.decisionjournal.model.Record;
 
 
 /**
@@ -37,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private LocalDate selectedDate = LocalDate.now();
     private TextView selectedDateTextView;
+
+    private RecyclerView.LayoutManager recyclerLayoutManager;
     private RecyclerView recordRecyclerView;
     private RecordRecyclerAdapter recordRecyclerAdapter;
 
@@ -54,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         calendarView = findViewById(R.id.calendar_view);
         selectedDateTextView = findViewById(R.id.toolbar_date_display);
         appBarLayout.setExpanded(appbarExpanded);
+
         recordRecyclerView = findViewById(R.id.record_recycler_view);
 
         recordRepository = new AsyncRecordRepository(getApplicationContext());
@@ -67,20 +65,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpRecyclerView() {
         recordRecyclerAdapter = new RecordRecyclerAdapter();
+        recyclerLayoutManager = new LinearLayoutManager(this);
         recordRecyclerView.setAdapter(recordRecyclerAdapter);
+        recordRecyclerView.setLayoutManager(recyclerLayoutManager);
+
+        handleDateChanged();
+
+        // Code for: Debug recyclerview
+//        Record r1 = new Record(DecisionEnum.WORKOUT, EmoEnum.HAPPY, currDate);
+//        Record r2 = new Record(DecisionEnum.STUDY, EmoEnum.HAPPY, new Date(currDate.getTime() + 1000));
+//        List<Record> list = new ArrayList<>();
+//        list.add(r1);
+//        list.add(r2);
+//        recordRecyclerAdapter.submitList(list);
+
+    }
+
+    // Observe new date when selected date changes.
+    private void handleDateChanged() {
         Date currDate = Date.valueOf(selectedDate.toString());
         recordRepository.getRecordOnDate(currDate).observe(this, records -> {
             recordRecyclerAdapter.submitList(records);
         });
-
     }
 
     private void setupCalendarListener() {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                selectedDate = LocalDate.of(i, i1+1, i2);
+                selectedDate = LocalDate.of(i, i1 + 1, i2);
                 setToolbarDate();
+                handleDateChanged();
                 // calendarview's date wont change upon date selection, only this listener is called.
 //                LocalDate newDate = Instant.ofEpochMilli(calendarView.getDate()).atZone(ZoneId.systemDefault()).toLocalDate();
 //                Log.i(TAG, String.format("in date listener, new date: %s", newDate.toString()));
