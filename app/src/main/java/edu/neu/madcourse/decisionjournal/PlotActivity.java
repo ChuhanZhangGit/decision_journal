0ae8b2aa9fd80986206e01b55f5047cb2f601593
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-09 21:50:39 -0500

    add fragments

---
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/PlotActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.decisionjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plot);
+    }
+}
