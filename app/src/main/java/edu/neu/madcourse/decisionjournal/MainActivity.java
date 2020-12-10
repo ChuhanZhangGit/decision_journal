3c7bfd5a065c89f86e78ba17c4419f767d674ca6
Chuhan Zhang <zhangchuhan1995@gmail.com>
2020-12-09 22:59:54 -0500

    shake sensor implemented, can trigger dialog

--- app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/MainActivity.java
@@ -3,11 +3,18 @@ package edu.neu.madcourse.decisionjournal;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.LinearLayout;
@@ -18,6 +25,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.Objects;
 
 import edu.neu.madcourse.decisionjournal.dao.AsyncRecordRepository;
 
@@ -35,13 +43,17 @@ public class MainActivity extends AppCompatActivity {
     private LocalDate selectedDate = LocalDate.now();
     private TextView selectedDateTextView;
 
-    private FloatingActionButton fab;
 
-    private RecyclerView.LayoutManager recyclerLayoutManager;
     private RecyclerView recordRecyclerView;
     private RecordRecyclerAdapter recordRecyclerAdapter;
 
     private AsyncRecordRepository recordRepository;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener mSensorListener;
+
+    DialogFragment catDialog = CatDialogFragment.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         appBarLayout = findViewById(R.id.app_bar_layout);
         calendarView = findViewById(R.id.calendar_view);
         selectedDateTextView = findViewById(R.id.toolbar_date_display);
-        fab = findViewById(R.id.fab);
 
         appBarLayout.setExpanded(appbarExpanded);
 
@@ -62,16 +73,72 @@ public class MainActivity extends AppCompatActivity {
 
         recordRepository = new AsyncRecordRepository(getApplicationContext());
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = Objects.requireNonNull(sensorManager).getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorListener = new MotionSensorListener();
+        sensorManager.registerListener(mSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         setupCalendarListener();
         // Initialize text view to select today's date. Calendarview by default initialize to today's
         // date.
         setToolbarDate();
         setUpRecyclerView();
+        Log.i(TAG, "after onCreate");
+    }
+
+    /**
+     * Listener for accelerometer sensor which listen to device shakes.
+     */
+    private class MotionSensorListener implements SensorEventListener {
+        // In this example, alpha is calculated as t / (t + dT),
+        // where t is the low-pass filter's time-constant and
+        // dT is the event delivery rate.
+        private final float alpha = 0.8f;
+        float[] gravity = new float[]{0, 0, 0};
+        float[] linear_acceleration = new float[3];
+        private float lastAcceleration = 0;
+        private float currAcceleration = 0;
+        private int THRESHOLD = 12;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            // Isolate the force of gravity with the low-pass filter.
+//            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+//            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+//            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+            // Remove the gravity contribution with the high-pass filter.
+//            float ax = event.values[0] - gravity[0];
+//            float ay = event.values[1] - gravity[1];
+//            float az = event.values[2] - gravity[2];
+            float ax = event.values[0];
+            float ay = event.values[1];
+            float az = event.values[2] - 9.85f;
+//            Log.i(TAG, String.format("gravity read in xyz: %f, %f, %f", gravity[0], gravity[1],
+//                    gravity[2]));
+//            Log.i(TAG, String.format("adjusted accerlation read in xyz: %f, %f, %f",
+//                    linear_acceleration[0], linear_acceleration[1], linear_acceleration[2]));
+            lastAcceleration = currAcceleration;
+            currAcceleration = ax * ax + ay * ay + az * az;
+            Log.i(TAG, String.format("Current accelation: %f, previous acceleration: %f",
+                    currAcceleration, lastAcceleration));
+            if (currAcceleration - lastAcceleration > THRESHOLD * THRESHOLD) {
+                Log.i(TAG, String.format("Triggered when Current accelation: %f, previous acceleration: %f",
+                        currAcceleration, lastAcceleration));
+                launchCatDialog();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
     }
 
     private void setUpRecyclerView() {
         recordRecyclerAdapter = new RecordRecyclerAdapter();
-        recyclerLayoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
         recordRecyclerView.setAdapter(recordRecyclerAdapter);
         recordRecyclerView.setLayoutManager(recyclerLayoutManager);
 
@@ -122,8 +189,17 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.fab:
                 startActivity(new Intent(this, CreateActivity.class));
+                break;
             case R.id.cat_dialog_bn:
-                CatDialogFragment.getInstance().show(getSupportFragmentManager(), "cat dialog");
+                launchCatDialog();
+                break;
+        }
+    }
+
+    private void launchCatDialog() {
+        if (catDialog.getDialog() == null ||
+                !catDialog.getDialog().isShowing()) {
+            catDialog.show(getSupportFragmentManager(), "cat dialog");
         }
     }
 }
