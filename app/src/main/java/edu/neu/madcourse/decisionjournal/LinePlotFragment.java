7241556525513901924b03e6571492794aad6436
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-09 22:08:33 -0500

    plot inside fragment

--- app/src/main/java/edu/neu/madcourse/decisionjournal/LinePlotFragment.java
+++ app/src/main/java/edu/neu/madcourse/decisionjournal/LinePlotFragment.java
@@ -2,11 +2,21 @@ package edu.neu.madcourse.decisionjournal;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,6 +63,8 @@ public class LinePlotFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -62,5 +74,30 @@ public class LinePlotFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_line_plot, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        lineChart = view.findViewById(R.id.line_emotion);
+
+        LineDataSet lineDataSet1 = new LineDataSet(dataValues(),"Data Set 1");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet1);
+
+        LineData lineData = new LineData(dataSets);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+    }
+
+    private LineChart lineChart;
+    private ArrayList<Entry> dataValues() {
+        ArrayList<Entry> dataVal = new ArrayList<>();
+        dataVal.add(new Entry(0, 20));
+        dataVal.add(new Entry(1, 24));
+        dataVal.add(new Entry(2, 50));
+        dataVal.add(new Entry(3, 10));
+        dataVal.add(new Entry(4, 28));
+        return dataVal;
+    }
 
 }
